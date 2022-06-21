0f049d2cc58b5ea013022eb37dda9ff8921ba621
kasperiapell <kasperi.apell@gmail.com>
2022-06-21 19:15:39 +0300

    initial commit

--- algorithms/sorting/comparison sorting/bubble sort/bubble_sort.cpp
+++ algorithms/sorting/comparison sorting/bubble sort/bubble_sort.cpp
@@ -1,17 +1,15 @@
 // bubble_sort.cpp
-#include <iostream>
 
 void bubble_sort_brute (int a[], int length)
 {
-  int n, m, x, y;
+  int n, m, x;
   
   for (n=0; n<length-1; n++) {
     for (m=0; m<length-1; m++) {
       x = a[m];
-      y = a[m+1];
     
-      if (x > y) {
-	a[m] = y;
+      if (x > a[m+1]) {
+	a[m] = a[m+1];
         a[m+1] = x;
       }
     }
@@ -20,16 +18,15 @@ void bubble_sort_brute (int a[], int length)
 
 void bubble_sort (int a[], int length)
 {
-  int n, m, x, y;
+  int n, m, x;
   
   for (n=0; n<length-1; n++) {
     // The second iterand does not need to go through all n components
     for (m=0; m<length-n-1; m++) {
       x = a[m];
-      y = a[m+1];
     
-      if (x > y) {
-	a[m] = y;
+      if (x > a[m+1]) {
+	a[m] = a[m+1];
         a[m+1] = x;
       }
     }
@@ -38,17 +35,16 @@ void bubble_sort (int a[], int length)
 
 void bubble_sort_optimised (int a[], int length)
 {
-  int n, m, x, y;
+  int n, m, x;
   bool swapped = false;
   
   for (n=0; n<length-1; n++) {
     // The second iterand does not need to go through all n components
     for (m=0; m<length-n-1; m++) {
       x = a[m];
-      y = a[m+1];
     
-      if (x > y) {
-	a[m] = y;
+      if (x > a[m+1]) {
+	a[m] = a[m+1];
         a[m+1] = x;
 	swapped = true;
       }
