545527249bf35683b261791e4837f589b563153b
kasperiapell <kasperi.apell@gmail.com>
2022-06-27 22:27:43 +0300

    initial commit

--- algorithms/sorting/comparison sorting/selection_sort/selection_sort.cpp
+++ algorithms/sorting/comparison sorting/selection_sort/selection_sort.cpp
@@ -1,21 +1,17 @@
-// bubble_sort_unittest.cpp
-
-
-
-
 // selection_sort.cpp
-#include <iostream>
 
 void selection_sort (int a [], int length) {
   int n, m, x;
 
   for (n=0; n<=length; n++) {
     int least = n;
+    
     for (m=n; m<=length; m++) {
       if (a[m]<a[least]) {
 	least = m;
       }
     }
+    
     if (n != least) {
       x = a[least];
       a[least] = a[n];
